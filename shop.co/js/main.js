d53b478ad8e20044786ee603dc06cf4a3f080781
maksim19821983-hash <maksim19821983@gmail.com>
2026-03-24 18:39:28 +0300

    Delete shop.co directory

--- shop.co/js/main.js
+++
@@ -1,8 +0,0 @@
-import { closeBanner } from './closeBanner.js';
-import { initBurger } from './burger.js';
-
-// Инициализация после загрузки DOM
-document.addEventListener('DOMContentLoaded', () => {
-    closeBanner();
-    initBurger();
-});
